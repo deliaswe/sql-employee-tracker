489c4863c5f70e331da2d10b86267c13530d81a5
delia <you@example.com>
2023-07-01 20:17:18 -0400

    Function to start the application

--- server.js
+++ server.js
@@ -35,3 +35,84 @@ cfonts.say('Delia Young \nSQL Employee Tracker', {
     transitionGradient: false,
     env: 'node'
 });
+
+// Function to start the application
+function start() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do? \n",
+            choices: [
+                "View all employees",
+                "View all departments",
+                "View all roles",
+                "Add an employee",
+                "Add a department",
+                "Add a role",
+                "add a manager",
+                "Update an employee role",
+                "View employees by manager",
+                "View employees by department",
+                "Delete | employee | role | department",
+                "View the total utilized budget of a department",
+                "Exit",
+            ],
+        })
+        .then((answer) => {
+            switch (answer.action) {
+                case "View all employees":
+                    viewEmployees();
+                    break;
+
+                case "View all departments":
+                    viewDepartments();
+                    break;
+
+                case "View all roles":
+                    viewRoles();
+                    break;
+
+                case "Add an employee":
+                    addEmployee();
+                    break;
+
+                case "Add a department":
+                    addDepartment();
+                    break;
+
+                case "Add a role":
+                    addRole();
+                    break;
+
+                case "Add a manager":
+                    addManager();
+                    break;
+
+                case "Update an employee role":
+                    updateRole();
+                    break;
+
+                    case "View employees by manager":
+                        viewByManager();
+                        break;
+
+                    case "View employees by department":
+                        viewByDepartment();
+                        break;
+
+                    case "Delete | employee | role | department":
+                        deleteEmployee();
+                        break;
+
+                    case "View the total utilized budget of a department":
+                        viewBudget();
+                        break;
+
+                case "Exit":
+                    connection.end();
+                    console.log("Goodbye!");
+                    break;
+            }
+        });
+}
